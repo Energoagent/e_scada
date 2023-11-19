a2d92f52b6002e2c79eab3b91afe4f3f883034f1
energoagent <kovalevvb@hotmail.com>
2023-11-19 20:52:04 +0300

    Floor chart addded, data simulation

--- e_scada/urls.py
+++ e_scada/urls.py
@@ -1,14 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from server import views
 
 urlpatterns = [
+    path('server/processor/', views.Processor.as_view()),
+    path('floor/processor/', views.ParameterFloorView.as_view()),
     path('', views.about),
-    path('processor', views.Processor.as_view()),
-    path('about/', views.about, name = 'about'),
+    path('server/', views.server),
+    path('floor/', views.floor),
+    path('about/', views.about),
     path('admin/', admin.site.urls),
-#    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- server/views.py
+++ server/views.py
@@ -5,10 +5,36 @@ from datetime import datetime
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from openpyxl import load_workbook
+
+parameters = {}
+
+try:
+    wb = load_workbook('config/parameters.xlsx')
+except Exception as e: 
+    print('EXCEPTION:', e)
+sheet = wb.active
+for i in range(2, sheet.max_row):
+    parameters[sheet.cell(row = i, column = 1).value] = str(sheet.cell(row = i, column = 2).value)
+
 def about(request):
     return render(request, 'index.html', 
         context = {
             'data_from_view': 'ESA SCADA', 
+            }
+        )
+
+def server(request):
+    return render(request, 'server.html', 
+        context = {
+            'data_from_view': 'ESA SCADA SERVER', 
+            }
+        )
+
+def floor(request):
+    return render(request, 'floor.html', 
+        context = {
+            'data_from_view': 'ESA SCADA SERVER', 
             }
         )
 
@@ -29,3 +55,11 @@ class Processor(APIView):
                     humidity = datetime.now().time().second
                     return Response({'Humidity': str(humidity)})
         return Response({'Error': 'undefined parameter type'})
+
+class ParameterFloorView(APIView):
+    def get(self, request):
+        if request.method == 'GET':
+            return Response({'parameters':parameters})
+        else:
+            return Response({'Error': 'invalid HTTP method'})
+
