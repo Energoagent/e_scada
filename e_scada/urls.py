bf0e79b99d16bcdba39dc86da3d311d8ae484604
energoagent <kovalevvb@hotmail.com>
2023-11-24 19:09:45 +0300

    realese local scada pages: page_1, page_2, page_3; transver and display parameter of cybro from database

--- e_scada/urls.py
+++ e_scada/urls.py
@@ -6,10 +6,14 @@ from server import views
 
 urlpatterns = [
     path('server/processor/', views.Processor.as_view()),
-    path('floor/processor/', views.ParameterFloorView.as_view()),
+    path('floor/processor/', views.ParameterFloor2View.as_view()),
+    path('graf/processor/', views.ParameterGrafView.as_view()),
+    path('floor/', views.floor),
     path('', views.about),
     path('server/', views.server),
-    path('floor/', views.floor),
+    path('graf/', views.graf),
+    path('report/', views.report),
+    path('report/import/', views.report_import),
     path('about/', views.about),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
--- server/views.py
+++ server/views.py
@@ -6,60 +6,167 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from openpyxl import load_workbook
+import xml.etree.ElementTree as ET
 
-parameters = {}
+import requests
+from requests.exceptions import HTTPError
 
-try:
-    wb = load_workbook('config/parameters.xlsx')
-except Exception as e: 
-    print('EXCEPTION:', e)
-sheet = wb.active
-for i in range(2, sheet.max_row):
-    parameters[sheet.cell(row = i, column = 1).value] = str(sheet.cell(row = i, column = 2).value)
+def server(request):
+    return render(request, 'server.html', 
+        context = {
+            'data_from_view': 'ESA SCADA',
+            'context': 'server',
+            }
+        )
 
 def about(request):
+    request.session['floor_id'] = 2,
+    request.session['unit_id'] = 214,
+    request.session.modified = True
     return render(request, 'index.html', 
         context = {
             'data_from_view': 'ESA SCADA', 
             }
         )
+def report_import(request):
+    if request.method == 'GET':
+        floor_id = request.session['floor_id']
+        unit_id = request.session['unit_id']
+    return redirect('../')
 
-def server(request):
-    return render(request, 'server.html', 
-        context = {
-            'data_from_view': 'ESA SCADA SERVER', 
-            }
-        )
+def report(request):
+    if request.method == 'GET':
+        floor_id = request.session['floor_id']
+        unit_id = request.session['unit_id']
+        return render(request, 'report.html', 
+            context = {
+                'data_from_view': 'ESA SCADA', 
+                'context': 'report',
+                'is_paginated' : 'true',
+                'page_number' : '1',
+                'floor_id': str(floor_id),
+                'unit_id': str(unit_id),
+                'contextmenu': {
+                    'Импорт': 'formmethod=GET formaction=../import/'},
+               }
+            )
+
+def graf(request):
+    if request.method == 'GET':
+        floor_id = request.session['floor_id']
+        unit_id = request.session['unit_id']
+        graf_number = int(request.GET.get('page_number'))
+        graf_change = request.GET.get('page_change')
+        match graf_change:
+            case 'to_home': graf_number = 1
+            case 'to_end': graf_number = 10
+            case '+1': 
+                graf_number = graf_number + 1
+                if graf_number > 10: graf_number = 10
+            case '-1': 
+                graf_number = graf_number - 1
+                if graf_number < 1: graf_number = 1
+        request.session['unit_id'] = unit_id
+        request.session.modified = True
+        return render(request, 'graf.html', 
+            context = {
+                'data_from_view': 'ESA SCADA', 
+                'context': 'graf',
+                'is_paginated' : 'true',
+                'page_number' : str(graf_number),
+                'floor_id': str(floor_id),
+                'unit_id': str(unit_id),
+               }
+            )
+    else:
+        return render(request, 'index.html', 
+            context = {
+                'data_from_view': 'invalid HTTP method',
+                }
+            )
 
 def floor(request):
-    return render(request, 'floor.html', 
+    if request.method == 'GET':
+        floor_number = int(request.GET.get('page_number'))
+        floor_change = request.GET.get('page_change')
+        match floor_change:
+            case 'to_home': floor_number = 1
+            case 'to_end': floor_number = 3
+            case '+1': 
+                floor_number = floor_number + 1
+                if floor_number > 3: floor_number = 3
+            case '-1': 
+                floor_number = floor_number - 1
+                if floor_number < 1: floor_number = 1
+        request.session['floor_id'] = floor_number
+        request.session.modified = True
         context = {
-            'data_from_view': 'ESA SCADA SERVER', 
+            'data_from_view': 'ESA SCADA', 
+            'context': 'floor',
+            'is_paginated': 'true',
+            'floor_id': floor_number,
             }
-        )
+        match floor_number:
+            case 1:
+                context['page_number'] = str(floor_number)
+                return render(request, 'common.html', context = context)
+            case 2:
+                context['page_number'] = str(floor_number)
+                return render(request, 'floor2.html', context = context)
+            case 3:
+                context['page_number'] = str(floor_number)
+                return render(request, 'floor3.html', context = context)
+        return render(request, 'index.html', 
+            context = {
+                'data_from_view': 'floor not found', 
+                'context': 'floor',
+                }
+            )
+    else:
+        return render(request, 'index.html', 
+            context = {
+                'data_from_view': 'invalid HTTP method', 
+                }
+            )
 
 class Processor(APIView):
     def get(self, request):
         if request.method == 'GET':
-            prmType = request.GET.get('prmType')
-            match prmType:
-                case 'Pressure':
-                    pressure = datetime.now().time().second
-                    return Response({'Pressure': str(pressure)})
-            match prmType:
-                case 'Temperature':
-                    temperature = datetime.now().time().minute
-                    return Response({'Temperature': str(temperature)})
-            match prmType:
-                case 'Humidity':
-                    humidity = datetime.now().time().second
-                    return Response({'Humidity': str(humidity)})
-        return Response({'Error': 'undefined parameter type'})
+            return Response({'TestData': 'TestData'})
+        return Response({'Error': 'invalid HTTP method'})
 
-class ParameterFloorView(APIView):
+class ParameterFloor2View(APIView):
     def get(self, request):
         if request.method == 'GET':
-            return Response({'parameters':parameters})
-        else:
-            return Response({'Error': 'invalid HTTP method'})
+            page_number = request.session['floor_id']
+            try:
+                resp = requests.post('http://192.168.22.252:8686/parameter/',
+                    timeout = 20,
+                    params = {'page': str(page_number)})
+            except Exception as e:
+                print('EXEPTION:', e)
+            else:
+                if resp != None: 
+                    parameters = resp.json()
+#                    print('DEBUG:', resp.headers)
+                    print('DEBUG:', resp.content)
+#                    print('DEBUG:', resp.json())
+                    parameters = resp.json()
+                    return Response({'parameters': parameters})
+        return Response({'Error': 'HTTP error'})
+
+class ParameterGrafView(APIView):
+    def get(self, request):
+        if request.method == 'GET':
+            try:
+                resp = requests.post('http://192.168.22.252:8686/parameter/',
+                    timeout = 1,
+                    data = {'name': 'mbdV214'})
+            except Exception as e:
+                print('EXEPTION:', e)
+            else:
+                if resp != None: 
+                    parameters = resp.json()
+                    return Response({'parameters': parameters})
+        return Response({'Error': 'HTTP error'})
 
